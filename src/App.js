ab40aefcf500e3c373c834be2159deb989c59dd1
Khalil Selyan <khalilselyan2@gmail.com>
2021-10-10 12:29:30 +0300

    Updates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component.jsx";
 
+const HatsPAge = () => {
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>;
+};
 function App() {
-  return <HomePage />;
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPAge} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
