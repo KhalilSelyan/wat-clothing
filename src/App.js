f705c33ee488b6ec391e01d175ae0a7786a0e228
KhalilSelyan <khalilselyan2@gmail.com>
2021-10-12 01:10:27 +0300

    Updates

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import SignInOut from "./pages/signinout/signinout.component";
 import Header from "./components/header/header.component";
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInOut} />
       </Switch>
     </div>
   );
---
+++ src/pages/signinout/signinout.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import SignIn from "../../components/signin/signin.component";
+import "./signinout.styles.scss";
+
+const SignInOut = () => (
+  <div className="signinup">
+    <SignIn />
+  </div>
+);
+
+export default SignInOut;
