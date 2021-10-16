ccc47715b4e7920633b692ce4d7902e60890daf3
KhalilSelyan <khalilselyan2@gmail.com>
2021-10-16 12:36:47 +0300

    added sign-up feature

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,6 +11,31 @@ const config = {
   appId: "1:97267834195:web:d9ddb5bdc7d9e50c7155a6",
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapshot = await userRef.get();
+
+  if (!snapshot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
--- src/pages/signinout/signinout.component.jsx
+++ src/pages/signinout/signinout.component.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import SignIn from "../../components/signin/signin.component";
+import SignUp from "../../components/sign-up/sign-up.component";
+
 import "./signinout.styles.scss";
 
 const SignInOut = () => (
   <div className="signinup">
     <SignIn />
+    <SignUp />
   </div>
 );
 
