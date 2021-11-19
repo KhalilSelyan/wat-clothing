98caa9583065d99c0ad4bc433270307f3adbc69d
KhalilSelyan <khalilselyan2@gmail.com>
2021-11-19 14:00:24 +0300

    updating stripe integration and updating our icon and title

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -7,6 +7,7 @@ import {
   selectCartItems,
   selectCartTotal,
 } from "../../redux/cart/cart.selectors";
+import StripeCheckoutButton from "react-stripe-checkout";
 
 import "./checkout.styles.scss";
 
@@ -34,6 +35,11 @@ const CheckoutPage = ({ cartItems, total }) => (
     ))}
     <div className="total">
       <span>TOTAL ${total}</span>
+      <div className="test-warning">
+        *Please use the following test credit card for payments* <br />
+        4242 4242 4242 4242 - EXP 01/20 - CVV: 123
+      </div>
+      <StripeCheckoutButton price={total} />
     </div>
   </div>
 );
